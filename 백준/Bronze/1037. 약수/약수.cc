ae19c9a8ff4c1de3ecd7eba1e05042e9b3d9e65b
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2024-09-15 21:30:01 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    std::vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        std::cin >> arr[i];
+    }
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < N; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    std::cout << min * max << std::endl;
+    return 0;
+}
