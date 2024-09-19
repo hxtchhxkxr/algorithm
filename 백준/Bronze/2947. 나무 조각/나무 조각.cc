75548956d9df0990115442d462faa4f40aafcf3e
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2024-09-19 20:09:43 +0900

    [Bronze I] Title: 나무 조각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.cc
+++ 백준/Bronze/2947. 나무 조각/나무 조각.cc
@@ -11,21 +11,18 @@ int main() {
     int result[5] = { 1, 2, 3, 4, 5 };
 
     while (!equal(arr, arr + 5, result)) {
-        int after[5];
-        copy(arr, arr + 5, after);
+        for (int i = 0; i < 4; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
 
-        for (int i = 0; i < 4; i++) { 
-            if (after[i] > after[i + 1]) {
-                int temp = after[i];
-                after[i] = after[i + 1];
-                after[i + 1] = temp;
                 for (int j = 0; j < 5; j++) {
-                    cout << after[j] << " ";
+                    cout << arr[j] << " ";
                 }
                 cout << "\n";
             }
         }
-        copy(after, after + 5, arr);
     }
 
     return 0;
