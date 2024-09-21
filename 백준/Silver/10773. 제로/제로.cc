d653ffd7059ffb4f142fdfaea8ac335befdc6b06
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2024-09-22 07:31:26 +0900

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int k;
+	cin >> k;
+
+	stack<int> s;
+
+	for (int i = 0; i < k; i++) {
+		int n;
+		cin >> n;
+
+		if (n == 0) 
+			s.pop();
+		else 
+			s.push(n);
+	}
+
+	int sum = 0;
+	while(!s.empty()) {
+		sum += s.top();
+		s.pop();
+	}
+
+	cout << sum;
+	return 0;
+}
