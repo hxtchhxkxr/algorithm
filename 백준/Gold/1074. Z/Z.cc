3cf564f0d877f9d63117a1dfa17839c4330a8022
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2025-02-13 04:59:02 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+using ll = long long;
+
+ll visit(int n, int r, int c) {
+	// base conditon
+	if (r == 0 && c == 0) return 0;
+
+	// recursion
+	int val = (1 << (n - 1)) * (1 << (n - 1));
+	if ((r >= 0 && r < (1 << n - 1)) && (c >= 0 && c < (1 << n - 1))) {
+		return visit(n - 1, r, c);
+	}
+	else if ((r >= 0 && r < (1 << n - 1)) && (c >= (1 << n - 1) && c < (1 << n))) {
+		return visit(n - 1, r, c - (1 << n - 1)) + val;
+	}
+	else if ((r >= (1 << n - 1) && r < (1 << n)) && (c >= 0 && c < (1 << n - 1))) {
+		return visit(n - 1, r - (1 << n - 1), c) + 2 * val;
+	}
+	else if ((r >= (1 << n - 1) && r < (1 << n)) && (c >= (1 << n - 1) && c < (1 << n))) {
+		return visit(n - 1, r - (1 << n - 1), c - (1 << n - 1)) + 3 * val;
+	}
+}
+
+int main() {
+	int n, r, c;
+	cin >> n >> r >> c;
+	cout << visit(n, r, c);
+}
