f6f0a2b30620e32c510fab438b0d89d70d274503
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2025-02-13 02:17:39 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -6,18 +6,10 @@ ll power(ll a, ll b, ll c) {
 	if (b == 1)return a % c;
 	ll num = power(a, b / 2, c);
 	ll numMod = (num * num) % c;
-	if (b % 2 == 0) {
-		return numMod;
-	}
-	else {
-		return (numMod * a) % c;
-	}
+	return (b % 2 == 0) ? numMod : (numMod * a) % c;
 }
 
 int main() {
-	ios::sync_with_stdio(0);	
-	cin.tie(0);
-
 	ll a, b, c;
 	cin >> a >> b >> c;
 	cout << power(a, b, c);
