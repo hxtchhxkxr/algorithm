2c6058e95feee835dbdf45d7de6d18544aec8b9b
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2025-03-23 20:23:48 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int cnt0[41];
+int cnt1[41];
+
+void fibonacciCount() {
+    cnt0[0] = 1; 
+    cnt1[0] = 0;
+
+    cnt0[1] = 0;
+    cnt1[1] = 1;
+
+    for (int i = 2; i <= 40; i++) {
+        cnt0[i] = cnt0[i - 1] + cnt0[i - 2];
+        cnt1[i] = cnt1[i - 1] + cnt1[i - 2];
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    fibonacciCount(); 
+
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << cnt0[n] << ' ' << cnt1[n] << '\n';
+    }
+}
