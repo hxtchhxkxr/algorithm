c9ff514dbea3de4f73e4be95b8576ce2adaf6786
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2025-04-02 21:33:11 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios::sync_with_stdio(0);	
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	priority_queue<ll, vector<ll>, greater<ll>> pq;
+
+	while (n--) {
+		int x;
+		cin >> x;
+
+		if (x == 0) {
+			if (!pq.empty()) {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else {
+				cout << 0 << '\n';
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
