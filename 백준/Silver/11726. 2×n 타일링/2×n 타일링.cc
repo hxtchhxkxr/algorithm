8febe188f809823ca3e6c450d637f21858b10ba0
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2025-04-16 21:25:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,29 +1,18 @@
 #include <iostream>
 using namespace std;
 
-const int MOD = 10007;
-long long comb[1002][1002];
-
-void calcCombination(int n) {
-    for (int i = 0; i <= n; i++) {
-        comb[i][0] = comb[i][i] = 1;
-        for (int j = 1; j < i; j++) {
-            comb[i][j] = (comb[i - 1][j - 1] + comb[i - 1][j]) % MOD;
-        }
-    }
-}
+int dp[1001];
 
 int main() {
-    int n;
-    cin >> n;
+	int n;
+	cin >> n;
 
-    calcCombination(n);  
+	dp[1] = 1;
+	dp[2] = 2;
 
-    int result = 0;
+	for (int i = 3; i <= 1000; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
 
-    for (int i = 0; i <= n / 2; i++) {  
-        result = (result + comb[n - i][i]) % MOD;
-    }
-
-    cout << result;
+	cout << dp[n];
 }
